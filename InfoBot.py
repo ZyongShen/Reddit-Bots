ddc4c346af5b5286adeedb0a62b0d7df5b08e966
ZyongShen <49699361+ZyongShen@users.noreply.github.com>
2019-06-08 21:28:38 -0400

    Add files via upload

---
+++ InfoBot.py
@@ -0,0 +1,31 @@
+import praw
+import pdb
+import re
+import os
+import random
+
+reddit = praw.Reddit('bot1')
+subreddit = reddit.subreddit("UConn")
+
+
+for submission in subreddit.hot(limit=5):
+    print("Title: ",submission.title)
+    print("Author: ",submission.author)
+    print("Upvotes: ",submission.ups)
+    print("Downvotes: ",submission.downs)
+
+
+if not os.path.isfile("posts_replied_to.txt"):
+    posts_replied_to = []
+else:
+    with open("posts_replied_to.txt", "r") as f:
+        posts_replied_to = f.read()
+        posts_replied_to = posts_replied_to.split("\n")
+        posts_replied_to = list(filter(None, posts_replied_to))
+
+for submission in subreddit.hot(limit=5):
+    if submission.id not in posts_replied_to:
+        if re.search("raptors lets get it", submission.title, re.IGNORECASE):
+            submission.reply("#insanity in toronto")
+            print("Bot replying to: ", submission.title)
+            posts_replied_to.append(submission.id)
